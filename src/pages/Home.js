57be2123495648dd553c513717e8f60ae636daa3
JohnBridge1 <bridgej161@gmail.com>
2024-08-06 16:41:29 +0100

    Home.js commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import supabase from "../config/supabaseClient";
+
+const Home = () => {
+  console.log(supabase);
+
+  return (
+    <div className="page Home">
+      <h2>Home</h2>
+    </div>
+  );
+};
