19b22aa19e3842b6e4521a24d19287baa6f21e78
JohnBridge1 <bridgej161@gmail.com>
2024-08-09 14:08:26 +0100

    fixing error with import.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
-import { supabase } from "src/config/supabaseClient";
+import { supabase } from "../../config/supabaseClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
