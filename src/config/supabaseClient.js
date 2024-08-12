32b87f1f45653512afcea2e1b0e8a470d47c855b
JohnBridge1 <bridgej161@gmail.com>
2024-08-12 15:03:47 +0100

    env.local added to gitignore

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -3,6 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase Anon Key:", supabaseAnonKey);
+
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Supabase URL or Anon Key is missing");
 }
