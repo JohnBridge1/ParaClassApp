fe7819a430b1ae5ff25cf11ce95967935b56eb93
JohnBridge1 <bridgej161@gmail.com>
2024-08-12 14:46:14 +0100

    new changes to supabaseClient.

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,7 +1,10 @@
-// config/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL or Anon Key is missing");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
