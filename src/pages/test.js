f920bd3bd796176e0faa08975d16a816d307fc09
JohnBridge1 <bridgej161@gmail.com>
2024-08-15 21:05:38 +0100

    Don't need the test.

--- src/pages/test.js
+++
@@ -1,9 +0,0 @@
-// src/pages/test.js
-export default function Test() {
-  return (
-    <div>
-      <p>Supabase URL: {process.env.NEXT_PUBLIC_SUPABASE_URL}</p>
-      <p>Supabase Anon Key: {process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}</p>
-    </div>
-  );
-}
